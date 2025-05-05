628ebc00db8750c096ea391a4c8e4f35be779ca2
Lorenna Aguiar Nunes <133262137+loreslei@users.noreply.github.com>
2025-05-05 16:52:48 -0300

    tried to explain the code

--- trabalho5/dijkstra.py
+++ trabalho5/dijkstra.py
@@ -12,26 +12,27 @@ grafo = {
 }
 
 def dijkstra(grafo, origem, destino):
-    fila = [(0, origem, [])] 
-    visitados = set()
+    fila = [(0, origem, [])] # a fial é uma tupla que recebe o custo atual (inicialmente 0), a cidade de origem e o caminho q foi percorrido
+    visitados = set() #Cria um conjunto para guardar os nós que já foram visitados. set é mais otimizado, ent é por isso q usamos ele
 
-    while fila:
-        (custo, atual, caminho) = heapq.heappop(fila)
+    while fila: # enquanto tiver que visitar os nos, continua executando
+        
+        (custo, atual, caminho) = heapq.heappop(fila) # tira o no com o menor custo acumulado da fila
 
-        if atual in visitados:
+        if atual in visitados: # Se o nó já foi visitado antes, pula ele para não repetir.
             continue
 
-        caminho = caminho + [atual]
-        visitados.add(atual)
+        caminho = caminho + [atual] #adicionando atual ao caminho
+        visitados.add(atual) #marcando como visitado
 
         if atual == destino:
-            return caminho, custo
+            return caminho, custo #se o atual ja for o destino, retorna logo caminho e custo
 
-        for vizinho, peso in grafo.get(atual, []):
-            if vizinho not in visitados:
+        for vizinho, peso in grafo.get(atual, []): #p cada vizinho do no atual (usa essa formatacao pq se digitar o nome da cidade errado, retorna uma lista vazia)
+            if vizinho not in visitados: # se o vizinho ainda nao tiver sido visitado, adiciona ele na fila de prioridade e calcula o novo custo
                 heapq.heappush(fila, (custo + peso, vizinho, caminho))
 
-    return None, float('inf')
+    return None, float('inf') # se nao der p chegar no infeliz retorna nenhum caminho e custo infinito
 
 
 origem = 'Benfica'
